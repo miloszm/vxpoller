e5df1b399e7b2816878f2a59db6147d90793798b
muszynskim <MuszynskiM@quindell.com>
2014-11-11 09:33:02 +0000

    initial commit

---
+++ src/main/java/com/mimu/HttpUtil.java
@@ -0,0 +1,57 @@
+package com.mimu;
+
+import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.HttpServerResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by miloszmuszynski on 10/11/2014.
+ */
+public class HttpUtil {
+
+    public static void sendHeaders(HttpServerResponse response, MultiMap headers){
+        List<String> headerList = sliceMultiMap(headers);
+        for (String headerStr: headerList) {
+            response.write(headerStr);
+        }
+    }
+
+    private static List<String> sliceMultiMap(MultiMap headers){
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String,String> e: headers.entries()){
+            list.add("<li>" + e.getKey() + ": " + e.getValue() + "</li>\n");
+        }
+        return list;
+    }
+
+    /**
+     * get int param if present or zero
+     */
+    public static int getIntParam(HttpServerRequest req, String paramName, int defaultValue){
+
+        int intParam;
+
+        String paramText = req.params().get(paramName);
+        if (paramText == null || paramText.isEmpty()){
+            intParam = defaultValue;
+        }
+        else {
+            try {
+                intParam = Integer.parseInt(paramText);
+            }
+            catch(NumberFormatException e){
+                intParam = defaultValue;
+            }
+
+        }
+
+        return intParam;
+    }
+
+
+
+}
---
+++ src/main/java/com/mimu/PollerVerticle.java
@@ -0,0 +1,87 @@
+package com.mimu;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.*;
+import org.vertx.java.platform.Verticle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by miloszmuszynski on 10/11/2014.
+ */
+public class PollerVerticle extends Verticle {
+
+    AtomicLong receivedCounter = new AtomicLong();
+    long pollMillis = 0L;
+
+    final static int MAX_POLL = 10;
+
+    public void start() {
+
+
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestHandler(new Handler<HttpServerRequest>() {
+            @Override
+            public void handle(HttpServerRequest req) {
+                receivedCounter.set(0L);
+                pollMillis = System.currentTimeMillis();
+
+                final int maxPoll = HttpUtil.getIntParam(req, "r", MAX_POLL);
+
+                String path = req.path();
+                req.response().setChunked(true);
+                req.response().setStatusCode(200);
+                req.response().write("<html><body>");
+                HttpUtil.sendHeaders(req.response(), req.headers());
+                req.response().putHeader("Content-Type", "text/html");
+                req.response().write("<h1>hi, your path is: " + path + " version=0008 maxPoll=" + maxPoll + "</h1>");
+                req.response().write("</body></html>");
+                req.response().end();
+
+                HttpClient client = vertx.createHttpClient().setHost("localhost").setPort(9111);//9199
+                for (int i = 0; i < maxPoll; i++) {
+                    doPolling(client, maxPoll);
+                }
+            }
+        });
+
+        server.listen(9191);
+
+    }
+
+    /**
+     * doPolling
+     */
+    private void doPolling(HttpClient client, final int maxPoll){
+
+
+        final HttpClientRequest request = client.post("/actionEndpoint?wsdl", new Handler<HttpClientResponse>() {
+            @Override
+            public void handle(HttpClientResponse resp) {
+                resp.bodyHandler(new Handler<Buffer>(){
+                    @Override
+                    public void handle(Buffer buffer) {
+                        if (receivedCounter.incrementAndGet() == maxPoll) {
+                            long millis = System.currentTimeMillis() - pollMillis;
+                            System.out.println("received " + maxPoll + " in " + millis + " millis  buffer = " + buffer);
+                        }
+                    }
+                });
+            }
+        });
+
+        request.setChunked(true);
+
+        request.write("bla");
+
+        request.end();
+
+    }
+
+}
