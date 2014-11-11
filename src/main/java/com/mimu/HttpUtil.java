91487819a71975406490a0a24bddf05b68d1fdbd
muszynskim <MuszynskiM@quindell.com>
2014-11-11 09:53:34 +0000

    cosmetic

--- src/main/java/com/mimu/HttpUtil.java
+++ src/main/java/com/mimu/HttpUtil.java
@@ -29,7 +29,7 @@ public class HttpUtil {
     }
 
     /**
-     * get int param if present or zero
+     * get int param if present or a default value
      */
     public static int getIntParam(HttpServerRequest req, String paramName, int defaultValue){
 
